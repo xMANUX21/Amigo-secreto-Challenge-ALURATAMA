2cec89421f28c025e6abfc12f1f8c5f0e0f1da86
xMANUX21 <manuelxd53@gmail.com>
2025-02-03 22:22:15 -0500

    Se crea la funcion agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+const amigos=[]
+
+const ingreso=document.getElementById("amigo")
+
+
+function agregarAmigo(){
+    if(ingreso.value==''){
+        alert("Por favor, inserte un nombre.")
+    }else{
+        amigos.push(ingreso.value)
+        alert("Amigo agregado")      
+    }
+    ingreso.value=''
+}
+
